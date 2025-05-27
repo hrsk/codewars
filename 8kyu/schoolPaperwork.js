2a2d9c95f56174a05ab1b1f38874d9654694597e
hrsk <hrsk.yegor@gmail.com>
2025-05-27 15:42:24 +0300

    kata: schoolPaperwork

---
+++ 8kyu/schoolPaperwork.js
@@ -0,0 +1,26 @@
+//INSTRUCTIONS:
+
+// Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
+// Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
+
+//EXAMPLE:
+// n= 5, m=5: 25
+// n=-5, m=5: 0
+
+//SOLUTIONS:
+
+//first
+
+function paperwork(n, m) {
+    return n > 0 & m > 0 && n * m
+}
+
+console.log(paperwork(0,0))
+
+// second
+
+function secondPaperwork(n, m) {
+    return n > 0 && m > 0 ? n * m : 0
+}
+
+console.log(secondPaperwork(10,20))
