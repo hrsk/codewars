e66f37f59572653e4a419e65460ce57330b42ec7
hrsk <hrsk.yegor@gmail.com>
2025-05-28 10:59:17 +0300

    kata: totalAmountOfPoints second solution

--- 8kyu/totalAmountOfPoints.js
+++ 8kyu/totalAmountOfPoints.js
@@ -59,4 +59,23 @@ function points(games) {
     return win + lose + draw
 }
 
-console.log(points(["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"]))
+console.log(points(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]))
+
+//second
+
+function secondPoints(games) {
+
+    let totalScores = 0;
+
+    games.map(game => {
+        if (game[0] === game[2]) {
+            totalScores += 1;
+        } else if (game[0] > game[2]) {
+            totalScores += 3;
+        }
+    })
+
+    return totalScores
+}
+
+console.log(secondPoints(["1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3"]))
