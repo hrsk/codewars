e1a8634ed00bd0420b9fc13b6128d9b6e74dc399
hrsk <hrsk.yegor@gmail.com>
2025-06-01 10:45:58 +0300

    kata: Function 1 - hello world

---
+++ 8kyu/helloWorld.js
@@ -0,0 +1,17 @@
+//INSTRUCTIONS:
+// Make a simple function called greet that returns the most-famous "hello world!".
+
+// STYLE POINTS:
+
+// Sure, this is about as easy as it gets.
+// But how clever can you be to create the most creative "hello world" you can think of?
+// What is a "hello world" solution you would want to show your friends?
+
+//SOLUTION:
+// Write a function "greet" that returns "hello world!"
+
+function greet(phrase = 'hello world!') {
+    return phrase
+}
+
+console.log(greet())
