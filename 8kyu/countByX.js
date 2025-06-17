02bee394f6090f3b7553367de41413518ab8e4b7
hrsk <hrsk.yegor@gmail.com>
2025-06-17 10:50:27 +0300

    kata: CountByX

---
+++ 8kyu/countByX.js
@@ -0,0 +1,31 @@
+//INSTRUCTIONS:
+
+// Create a function with two arguments that will return an array of the first n multiples of x.
+//
+// Assume both the given number and the number of times to count will be positive numbers greater than 0.
+//
+// Return the results as an array or list ( depending on language ).
+
+//EXAMPLE:
+// x = 1, n = 10 --> [1,2,3,4,5,6,7,8,9,10]
+// x = 2, n = 5  --> [2,4,6,8,10]
+
+// SOLUTION:
+
+//first
+function countBy(x, n) {
+    //x -- данному значению должны быть кратны полученные числа
+    //n -- число, которое задает длину результируещего массива
+    let result = [];
+
+    for (let i = 1; i <= n; i++) {
+        //TODO: цикл начинает с i = 1, потому что краные числа начинаются с x * 1, а с не x * 0
+        result.push(x * i)
+
+    }
+
+    return result;
+}
+
+console.log(countBy(1, 10)) // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log(countBy(2, 5)) // [2, 4, 6, 8, 10]
