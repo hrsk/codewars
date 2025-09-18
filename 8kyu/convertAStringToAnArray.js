74ba95c6598d45b8401bc7c69ba3674f79b3df84
hrsk <hrsk.yegor@gmail.com>
2025-09-18 08:44:51 +0300

    kata: Convert a string to an array

---
+++ 8kyu/convertAStringToAnArray.js
@@ -0,0 +1,21 @@
+// INSTRUCTION:
+
+
+// EXAMPLE:
+
+// "Robin Singh" ==> ["Robin", "Singh"]
+// "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
+
+// SOLUTION:
+
+function stringToArray(string){
+
+    // code code code
+
+    return string.split(" ")
+
+}
+
+
+console.log(stringToArray("Robin Singh")) // ["Robin", "Singh"]
+console.log(stringToArray("I love arrays they are my favorite")) // ["Robin", "Singh"]
