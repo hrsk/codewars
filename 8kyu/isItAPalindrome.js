0bd6e95c2a16d1810edb2fa0763c6d6b068dbafc
hrsk <hrsk.yegor@gmail.com>
2025-09-24 11:42:56 +0300

    kata: Is it a palindrome?

---
+++ 8kyu/isItAPalindrome.js
@@ -0,0 +1,13 @@
+// DESCRIPTION:
+
+// Write a function that checks if a given string (case insensitive) is a palindrome.
+// A palindrome is a word, number, phrase, or other sequence of symbols
+// that reads the same backwards as forwards, such as madam or racecar.
+
+function isPalindrome(x) {
+    return x.toLowerCase() === x.toLowerCase().split('').reverse().join('')
+}
+
+console.log(isPalindrome("hello")) // false
+console.log(isPalindrome("Madam")) // true
+console.log(isPalindrome("AbBa")) // true
