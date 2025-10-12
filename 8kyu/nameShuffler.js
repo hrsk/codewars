4e14bf8a62e5a1e9378a5f87b3b427e07be66340
hrsk <hrsk.yegor@gmail.com>
2025-10-12 08:50:05 +0300

    kata: Name Shuffler

---
+++ 8kyu/nameShuffler.js
@@ -0,0 +1,16 @@
+// INSTRUCTION:
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+//EXAMPLE:
+// "john McClane" --> "McClane john"
+
+
+//SOLUTION:
+function nameShuffler(str){
+    return str.split(' ').reverse().join(' ')
+}
+
+console.log(nameShuffler('john McClane')) // "McClane john"
+console.log(nameShuffler('tom jerry')) // "jerry tom"
+
