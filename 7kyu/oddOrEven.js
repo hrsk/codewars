559b8371142d6d44c16b826a7450e4ff7d2871a9
hrsk <hrsk.yegor@gmail.com>
2025-10-14 09:41:51 +0300

    kata: Odd or Even?

---
+++ 7kyu/oddOrEven.js
@@ -0,0 +1,36 @@
+//DESCRIPTION:
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+//
+// Give your answer as a string matching "odd" or "even".
+//
+// If the input array is empty consider it as: [0] (array with a zero).
+
+//EXAMPLE:
+
+// Input: [0]
+// Output: "even"
+//
+// Input: [0, 1, 4]
+// Output: "odd"
+//
+// Input: [0, -1, -5]
+// Output: "even"
+
+//SOLUTION:
+function oddOrEven(array) {
+    if (!array.length) return "even"
+
+    let result = 0
+    for (let i = 0; i < array.length; i++) {
+        result += array[i]
+    }
+
+    return result % 2 === 0 ? 'even' : 'odd'
+}
+
+console.log(oddOrEven([0])) // "even"
+console.log(oddOrEven([1])) // "odd"
+console.log(oddOrEven([])) // "even"
+console.log(oddOrEven([0, 1, 5])) // "even"
+console.log(oddOrEven([0, 1, 3])) // "even"
+console.log(oddOrEven([1023, 1, 2])) // "even"
