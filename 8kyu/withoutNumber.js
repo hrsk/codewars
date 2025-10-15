a4bb4047941741248dc1214b9085ec7a4b5349e7
hrsk <hrsk.yegor@gmail.com>
2025-10-15 09:18:54 +0300

    kata: 5 without numbers !!

---
+++ 8kyu/withoutNumber.js
@@ -0,0 +1,13 @@
+//DESCRIPTION:
+
+// Write a function that always returns 5
+//
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// SOLUTION:
+
+function unusualFive() {
+    return 'hello'.length;
+}
+
+console.log(unusualFive()) // 5
