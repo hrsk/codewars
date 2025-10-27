35a3065f13a6665161ec5c12b65f1785fa244fdf
hrsk <hrsk.yegor@gmail.com>
2025-10-27 21:11:09 +0300

    kata: Find Maximum and Minimum Values of a List

---
+++ 8kyu/findMaximumAndMinimumValues.js
@@ -0,0 +1,27 @@
+//DESCRIPTION:
+// Your task is to make two functions ( max and min, or maximum and minimum, etc., depending on the language ) that receive a list of integers as input,
+// and return the largest and lowest number in that list, respectively. Each function returns one number.
+
+//EXAMPLES:
+// * [4,6,2,1,9,63,-134,566]         -> max = 566, min = -134
+// * [-52, 56, 30, 29, -54, 0, -110] -> min = -110, max = 56
+// * [42, 54, 65, 87, 0]             -> min = 0, max = 87
+// * [5]                             -> min = 5, max = 5
+
+//SOLUTION:
+
+var min = function (list) {
+    return list.sort((a, b) => a - b)[0]
+    // return list[0];
+}
+
+var max = function(list){
+
+    return list.sort((a, b) => b - a)[0]
+
+}
+
+console.log(min([-52, 56, 30, 29, -54, 0, -110])) // -110
+console.log(min([42, 54, 65, 87, 0])) // 0
+console.log(max([4,6,2,1,9,63,-134,566])) // 566
+console.log(max([5])) // 5
