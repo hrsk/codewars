53005e16c39d1769411179dbab122fe6b9797dfe
hrsk <hrsk.yegor@gmail.com>
2025-10-27 11:35:24 +0300

    kata: Add Length

---
+++ 8kyu/addLength.js
@@ -0,0 +1,19 @@
+//DESCRIPTION:
+
+// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+
+//EXAMPLES:
+
+// "apple ban" --> ["apple 5", "ban 3"]
+// "you will win" -->["you 3", "will 4", "win 3"]
+
+// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element.
+
+//SOLUTION:
+
+function addLength(str) {
+    return str.split(" ").map(w => `${w} ${w.length}`)
+}
+
+console.log(addLength("apple ban")) // ["apple 5", "ban 3"]
+console.log(addLength("you will win")) // ["you 3", "will 4", "win 3"]
