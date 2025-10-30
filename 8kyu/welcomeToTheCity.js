8b587e462bbb5c80879a4eb72d514c6f56ae89a9
hrsk <hrsk.yegor@gmail.com>
2025-10-30 13:53:47 +0300

    kata: Welcome to the City

---
+++ 8kyu/welcomeToTheCity.js
@@ -0,0 +1,17 @@
+//DESCRIPTION:
+
+// Create a method that takes as input a name, city, and state to welcome a person.
+// Note that name will be an array consisting of one or more values that should be joined together
+// with one space between each, and the length of the name array in test cases will vary.
+
+//EXAMPLES:
+// ['John', 'Smith'], 'Phoenix', 'Arizona'
+// This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+
+// SOLUTION:
+
+function sayHello( name, city, state ) {
+    return `Hello, ${name.join(" ")}! Welcome to ${city}, ${state}!`
+}
+
+console.log(sayHello(['Wallace','Russel','Osbourne'],'Albany','New York')) // 'Hello, Wallace Russel Osbourne! Welcome to Albany, New York!'
