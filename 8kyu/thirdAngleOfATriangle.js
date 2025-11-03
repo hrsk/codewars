271c48f3bf2c51f3ff8bb3681d9ee02b1c6ce381
hrsk <hrsk.yegor@gmail.com>
2025-11-03 11:32:48 +0300

    kata: Third Angle of a Triangle

---
+++ 8kyu/thirdAngleOfATriangle.js
@@ -0,0 +1,14 @@
+//DESCRIPTION:
+
+// You are given two interior angles (in degrees) of a triangle.
+// Write a function to return the 3rd.
+// Note: only positive integers will be tested.
+
+//SOLUTION:
+
+function otherAngle(a, b) {
+    return 180 - a - b;
+}
+
+console.log(otherAngle(30, 60)) // 90
+console.log(otherAngle(60, 60)) // 60
