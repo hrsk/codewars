3ce06521446d4614947744ecfd889c16ee98d44a
hrsk <hrsk.yegor@gmail.com>
2025-11-10 10:32:21 +0300

    kata: Grasshopper - Messi Goals

---
+++ 8kyu/grasshopperMessiGoals.js
@@ -0,0 +1,28 @@
+//DESCRIPTION:
+
+// Use variables to find the sum of the goals Messi scored in 3 competitions
+//
+// Messi goal scoring statistics:
+// Competition 	Goals
+// La Liga 	43
+// Champions League 	10
+// Copa del Rey 	5
+
+//TASK:
+//Create these three variables and store the appropriate values using the table above:
+//
+//laLigaGoals
+//championsLeagueGoals
+//copaDelReyGoals
+//
+//Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+
+//SOLUTION:
+
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
+
+console.log(totalGoals)
