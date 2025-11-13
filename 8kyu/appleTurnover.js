87c38919b033627e1dd394527f01bbc96ab70f17
hrsk <hrsk.yegor@gmail.com>
2025-11-13 11:25:32 +0300

    kata: Alan Partridge II - Apple Turnover

---
+++ 8kyu/appleTurnover.js
@@ -0,0 +1,16 @@
+// DESCRIPTION:
+
+// Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+//
+// NOTE:
+// Input will either be a positive integer (or a string for untyped languages).
+
+//SOLUTION:
+
+function apple(x) {
+
+    return (Number(x) ** 2) > 1000 ? `It's hotter than the sun!!` : 'Help yourself to a honeycomb Yorkie for the glovebox.'
+}
+
+console.log(apple('50'))
+console.log(apple(4))
