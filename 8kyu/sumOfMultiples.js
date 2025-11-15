2f87a24f64981d6bc273200263d2a7c16da94f9a
hrsk <hrsk.yegor@gmail.com>
2025-11-15 10:06:12 +0300

    kata: Sum of Multiples

---
+++ 8kyu/sumOfMultiples.js
@@ -0,0 +1,35 @@
+//DESCRIPTION:
+//Find the sum of all multiples of n below m
+
+// Keep in Mind
+//
+//     n and m are natural numbers (positive integers)
+//     m is excluded from the multiples
+//
+// EXAMPLES
+//
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+function sumMul(n,m){
+
+    if (n <= 0 || m <= 0) return "INVALID"
+
+    let result = 0;
+
+    for (let i = n; i < m; i+=n) {
+        if (i % n === 0)   {
+            result += i
+        }
+    }
+    return result
+}
+
+console.log(sumMul(4, -7))
+console.log(sumMul(0, 0))
+console.log(sumMul(4, 123))
+console.log(sumMul(2, 9))
+console.log(sumMul(3, 13))
+
